0dc443922417f2e3bb1989305b0d0ab3ecd0e62c
FionaMeuter <fiona030505@gmail.com>
2025-08-20 13:13:08 +0200

    Überprüfung der Boxgrenzen durch Simulation eines einzigen Teilchens, Ergebniss:keine elastische Reflexion, ohne vuisuelle Darstellung

--- Teilchen.py
+++ Teilchen.py
@@ -11,3 +11,19 @@ class Particle:
             self.position[i] + self.velocity [i]* timestep
             for i in range(len(self.position))
         ]
+    def check_boundary(self, box_size):
+        for i in range(len(self.position)):
+            if self.position[i] < 0 or self.position[i] > box_size[i]:
+                self.velocity[i] *= -1
+
+
+#Beispielwerte Teilchen in 2D Box              
+box_size = [10, 10] #Größe der Box
+particle = Particle(position=[5, 5], velocity=[1, -1], mass=1.0)
+
+# Simulation für 10 Zeitschritte um zu gucken wie das Teilchen sich mit den Grenzen verhält
+timestep = 1
+for step in range(10):
+    particle.move(timestep)
+    particle.check_boundary(box_size)
+    print(f"Schritt {step + 1}: Position = {particle.position}, Geschwindigkeit = {particle.velocity}")
